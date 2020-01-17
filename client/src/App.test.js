d2ac48801aa89dc05e38a4f9f887a5d78bf6842d
sooshe <sooshe@sooshebot.com>
2020-01-17 13:11:49 -0600

    clean components file for final push

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, findByDisplayValue } from "@testing-library/react";
 import App from './App';
 import WCPlayer from './components/WCPlayer';
 import WCPlayerCard from './components/WCPlayerCard';
+import { text } from 'body-parser';
 
 
 test('renders without crashing', () => {
@@ -17,4 +18,22 @@ test('header text is displaying', () => {
 test('WCPlayers data is displaying', () => {
   const { getByTestId } = render(<App />);
     getByTestId(/wc-player-container/i);
+});
+
+// test("App renders with light mode default", () => {
+//   const { getByTestId } = render(<App />);
+//   expect(getByTestId("dark-mode__toggle")).toBeInTheDocument();
+//   // expect(getByTestId("header")).toHaveStyle("background-color: white");
+//   expect(getByTestId).toHaveStyle("background-color:white"); 
+
+
+// });
+
+test("toggles the theme", () => {
+  const { getByTestId } = render(<App />);
+  const toggleBtn = getByTestId("toggle-theme-btn");
+  fireEvent.click(toggleBtn);
+  expect(getByTestId("header")).toHaveStyle("background-color: black");
+  fireEvent.click(toggleBtn);
+  expect(getByTestId("header")).toHaveStyle("background-color: white");
 });
